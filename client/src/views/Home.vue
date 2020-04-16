1129927a95b6f570229d361feaeee68ddfc193e8
RuliSlim <ruli0to8@gmail.com>
2020-04-16 19:47:48 +0700

    temporary

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,6 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Youdjas</h1>
+    <audio controls autoplay>
+      <source src="https://p.scdn.co/mp3-preview/dd64cca26c69e93ea78f1fff2cc4889396bb6d2f">
+    </audio>
   </div>
 </template>
 
